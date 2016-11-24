34d750c518896c3da0e0e1c8eb09316a1b61dc72
Arsen Sisengaliev <arsen.sisengaliev@ibillboard.com>
2016-11-24 12:44:46 +0100

    modify findDockerfiles.sh

--- findDockerfiles.sh
+++ findDockerfiles.sh
@@ -2,13 +2,12 @@
 
 TRAVIS_COMMIT_RANGE="e5bdd1a..410f772"
 COMMITTED_FILES=`git diff --name-only $TRAVIS_COMMIT_RANGE`
-#echo $COMMITTED_FILES
+echo $COMMITTED_FILES
 
 DOCKER_IMAGE_DIRS=`find . -name Dockerfile -printf '%h\n'`
 
 for dockerdir in $DOCKER_IMAGE_DIRS; do
-#    if [[ "$dockerdir" == "$COMMITTED_FILES" ]]; then
-    if [[ "ne dave" == "dave" ]]; then
+    if [[ $COMMITTED_FILES == *"$dockerdir"* ]]; then
         echo "Image $dockerdir needs to be built"
     fi
 done
