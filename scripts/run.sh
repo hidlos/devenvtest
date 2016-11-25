a985cbe6e1ee79571377e7e554ea62a3d403e951
Arsen Sisengaliev <arsen.sisengaliev@ibillboard.com>
2016-11-25 15:32:58 +0100

    refactor run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,12 +3,11 @@
 #set -ev
 #TRAVIS_COMMIT_RANGE="e17e329..4ffc20f"
 
-ROOTPATH=$PWD
+ROOT_PATH=$PWD
 COMMITTED_FILES=`git diff --name-only $TRAVIS_COMMIT_RANGE`
 
-DOCKER_DIRS=`bash scripts/getDirsForBuild.sh "$COMMITTED_FILES"`
-bash scripts/buildImages.sh "$DOCKER_DIRS" "$ROOTPATH"
-
 NODE_MODULE_DIRS=`bash scripts/getAffectedNodeModuleDirs.sh "$COMMITTED_FILES"`
-bash scripts/runTests.sh "$NODE_MODULE_DIRS" "$ROOTPATH"
+bash scripts/runTests.sh "$NODE_MODULE_DIRS" "$ROOT_PATH"
 
+DOCKER_DIRS=`bash scripts/getDirsForBuild.sh "$COMMITTED_FILES"`
+bash scripts/buildImages.sh "$DOCKER_DIRS" "$ROOT_PATH"
