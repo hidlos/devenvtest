e03e77069eb17cec1be746844c6cd24d9c97a00c
Arsen Sisengaliev <arsen.sisengaliev@ibillboard.com>
2016-11-25 15:25:06 +0100

    refactor building of docker images

--- scripts/buildImages.sh
+++ scripts/buildImages.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
 DOCKER_IMAGE_DIRS=$1
-ROOTPATH=$2
+ROOT_PATH=$2
+
+function getNodeModuleVersion() {
+    NODE_MODULE_VERSION=`grep -Po '(?<="version". ")[^"]*' package.json`;
+    if [ "$NODE_MODULE_VERSION" == "0.0.0-development" ]; then
+        echo 'latest';
+    else
+        echo $NODE_MODULE_VERSION;
+    fi
+}
 
 for DIR in $DOCKER_IMAGE_DIRS
 do
-	echo "Image $DIR needs to be built"
+	echo "Building and publishing Docker image $DIR"
+	IMAGE_VERSION=$DIR-$(getNodeModuleVersion)
+	echo "Image will be tagged with $IMAGE_VERSION"
+	docker build -t arsi/devenvtest:$IMAGE_VERSION .;
+	docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+    docker push arsi/devenvtest:$IMAGE_VERSION;
 done
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 #set -ev
-TRAVIS_COMMIT_RANGE="e17e329..4ffc20f"
+#TRAVIS_COMMIT_RANGE="e17e329..4ffc20f"
 
 ROOTPATH=$PWD
 COMMITTED_FILES=`git diff --name-only $TRAVIS_COMMIT_RANGE`
-DIRS_FOR_BUILD=`bash scripts/getDirsForBuild.sh "$COMMITTED_FILES"`
-AFFECTED_NODE_MODULE_DIRS=`bash scripts/getAffectedNodeModuleDirs.sh "$COMMITTED_FILES"`
 
-bash scripts/runTests.sh "$AFFECTED_NODE_MODULE_DIRS" "$ROOTPATH"
-bash scripts/buildImages.sh "$DIRS_FOR_BUILD" "$ROOTPATH"
+DOCKER_DIRS=`bash scripts/getDirsForBuild.sh "$COMMITTED_FILES"`
+bash scripts/buildImages.sh "$DOCKER_DIRS" "$ROOTPATH"
+
+NODE_MODULE_DIRS=`bash scripts/getAffectedNodeModuleDirs.sh "$COMMITTED_FILES"`
+bash scripts/runTests.sh "$NODE_MODULE_DIRS" "$ROOTPATH"
+
