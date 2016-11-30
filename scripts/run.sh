3984eefca524e6175c5b4221fbc152fb7f453655
adci <adam.cink@ibillboard.com>
2016-11-30 14:40:01 +0100

    run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,5 +9,12 @@ COMMITTED_FILES=`git diff --name-only $TRAVIS_COMMIT_RANGE`
 NODE_MODULE_DIRS=`bash scripts/getAffectedNodeModuleDirs.sh "$COMMITTED_FILES"`
 bash scripts/runTests.sh "$NODE_MODULE_DIRS" "$ROOT_PATH"
 
+if [ -f /here ]; then
+    echo "JE TAM";
+else
+    echo "NENI TAM";
+fi
+
+
 DOCKER_DIRECTORIES=`bash scripts/getDirsForBuild.sh "$COMMITTED_FILES"`
 bash scripts/buildImages.sh "$DOCKER_DIRECTORIES" "$ROOT_PATH"
