1c81a5f2a1ef6bf6f57ad66e93c6eb9816c6d4f1
duha <david.havrlant@ibillboard.com>
2016-12-07 14:21:27 +0100

    otevřen port

---
+++ apps/app1/index.js
@@ -0,0 +1,11 @@
+var http = require("http");
+
+function onResponse(request, response)
+{
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+
+    response.end("Ahoj :)");
+}
+
+var server = http.createServer(onResponse);
+server.listen(80);
