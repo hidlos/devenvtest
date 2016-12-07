7b3d5943b5570bbf2f3e9179959b4bc4a4f6be5f
adci <adam.cink@ibillboard.com>
2016-12-07 08:56:16 +0100

    fix

--- scripts/getLastSuccessfulBuildHash.sh
+++ scripts/getLastSuccessfulBuildHash.sh
@@ -3,4 +3,4 @@
 XML_URL=localhost:8080/job/pipe/lastSuccessfulBuild/api/xml
 XML_PATH='//workflowRun/action[@_class=\"hudson.plugins.git.util.BuildData\"]/lastBuiltRevision/SHA1/text()'
 LAST_SUCCESSFUL_BUILD_HASH=curl GET $XML_URL | xmllint --xpath '$XML_PATH' -
-echo LAST_SUCCESSFUL_BUILD_HASH
+echo $LAST_SUCCESSFUL_BUILD_HASH
