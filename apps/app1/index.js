55da74e47d18e1eea219334c9bbcc428aa70b70d
adci <adam.cink@ibillboard.com>
2016-12-08 08:21:46 +0100

    + console log

--- apps/app1/index.js
+++ apps/app1/index.js
@@ -1,10 +1,12 @@
 var http = require("http");
 
-function onResponse(request, response)
-{
+function onResponse(request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
+    response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Ahoj :)");
 }
 
 var server = http.createServer(onResponse);
 server.listen(9233);
+
+console.log("Server running at http://127.0.0.1:9233/");
